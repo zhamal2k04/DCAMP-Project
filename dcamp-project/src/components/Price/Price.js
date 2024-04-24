d72bd5a8852650be7858b1467cedb7a3e311eec0
Botirjon007 <batyrzhanulmasov@gmail.com>
2024-04-24 07:13:18 +0500

    price container set adn added to publick png image for background image

--- dcamp-project/src/components/Price/Price.js
+++ dcamp-project/src/components/Price/Price.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const Price = () => {
-  return (
-    <div className=''>Price</div>
-  )
+  return <div className='absolute mt-44 ml-36 rounded-3xl w bg-blue-500' style={{ width: "1160px", height: "974px",}}>Price</div>;
 }
 
 export default Price
